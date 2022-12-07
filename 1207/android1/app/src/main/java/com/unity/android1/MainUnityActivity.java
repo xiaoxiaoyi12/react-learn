aa1fec63a692af72d6c8433db14cbd2beb420f72
王娟娟 <wangjuanjuan@xiaomi.com>
2022-12-07 20:31:13 +0800

    feat: first commit

---
+++ 1207/android1/app/src/main/java/com/unity/android1/MainUnityActivity.java
@@ -0,0 +1,14 @@
+package com.unity.android1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainUnityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_unity);
+    }
+}
